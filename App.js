4b887bedba5f73bc3c6ab304dcd68ceca7d72e83
sciencezone67-lab <sciencezone67@gmail.com>
2025-09-21 00:50:28 +0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import ScienceZoneLandingPage from './components/science_zone_landing_page_react';
+
+function App() {
+  return (
+    <div className="App">
+      <ScienceZoneLandingPage />
+    </div>
+  );
+}
+
+export default App;
